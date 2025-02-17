335ea67b82ead613ddf2ebff6a1e3d82ccfb1c6d
Build_with_MJ <miraj.dinajpur@gmail.com>
2025-02-18 04:58:56 +0600

    Convert_temper_centigrade_to_degreature_from_degreee_Fahrenheit.c

--- Task-1/Convert_temperature_from_degree_centigrade_to_degree_Fahrenheit.c
+++ Task-1/Convert_temperature_from_degree_centigrade_to_degree_Fahrenheit.c
@@ -1,1 +1,10 @@
-
+#include<stdio.h>
+int main()
+{
+    float centigrade;
+    printf("Write the temperature in Centigrade : ");
+    scanf("%f",&centigrade);
+    float fahrenheit = ((centigrade*9)/5)+32;
+    printf("The Fahrenheit temperature is : %.2f",fahrenheit);
+    return 0;
+}
