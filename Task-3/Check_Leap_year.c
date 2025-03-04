c8d093a46da997f1b9734cbe4facc2c5865c6a92
Build_with_MJ <miraj.dinajpur@gmail.com>
2025-03-05 01:59:48 +0600

    Check_Leap_year

---
+++ Task-3/Check_Leap_year.c
@@ -0,0 +1,21 @@
+/*Write a C program to check if a year is Leap year or not*/
+
+#include<stdio.h>
+int main()
+{
+    int year;
+    printf("Enter a Year : ");
+    while(scanf("%d",&year) == 1)
+    {
+        if((year%4==0 && year%100!=0)||year%400==0)
+        {
+            printf("The year %d is a Leap year\n",year);
+        }
+        else
+        {
+            printf("The year %d is not a Leap year\n",year);
+        }
+    }
+
+    return 0;
+}
